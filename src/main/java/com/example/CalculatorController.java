483300974dcec1a12939af40da7fd4072db639c2
liam <liamtyler1010@gmail.com>
2024-09-30 10:19:55 -0400

    Added control methods

--- src/main/java/com/example/CalculatorController.java
+++ src/main/java/com/example/CalculatorController.java
@@ -1,4 +1,5 @@
 package com.example;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
@@ -55,6 +56,80 @@ public class CalculatorController {
 
     @FXML
     private Button ZeroButton;
+    
+    @FXML
+    void ZeroButtonPressed(ActionEvent event) {
 
+    }
+
+    @FXML
+    void OneButtonPressed(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    void TwoButtonPressed(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void ThreeButtonPressed(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    void FourButtonPressed(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    void FiveButtonPressed(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    void SixButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SevenButtonPressed(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void EightButtonPressed(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void NineButtonPressed(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void EuqalsButtonPressed(ActionEvent event) {
+        
+    }
+     
+    @FXML
+    void PlusButtonPressed(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void MinusButtonPressed(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    void MultiplyButtonPressed(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    void DivideButtonPressed(ActionEvent event) {
+
+    }
     
 }
